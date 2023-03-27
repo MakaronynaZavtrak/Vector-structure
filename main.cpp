4d6f839e3afc845d74d4d2ec06c5b53c3b642f15
ElectricGtr <114348027+MakaronynaZavtrak@users.noreply.github.com>
2023-03-27 11:16:09 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,58 +1,63 @@
-#include "VectorStruct.cpp"
 #include <iostream>
 #include <math.h>
-#include "Names.h"
+#include "Header.h"
 #include <string>
 using namespace std;
-int summArr(int arr[]) {
-	int result = 0;
-	for (int i = 0; i < 3; i++) {
-		result += *(arr + i);
-	};
-	return result;
-}
+
 int main()
 {
 	int c[3]{ 3,1, 2 };
 	int b[3]{ 2,5,2 };
 	int d[3]{ 2,2,2 };
-		Vectors M;
+	Vectors M;
+	M.printf("vector c: ");
+	M.printNewVector(c);
+	M.printfEndl("");
+
+	M.printf("vector b: ");
+	M.printNewVector(b);
+	M.printfEndl("");
+
+	M.printf("vector d: ");
+	M.printNewVector(d);
+	M.printfEndl("");
+
 	double x = M.findLength(c);// нахождения длины вектора
-	printf("length is: ");
-	printfEndl(x);
+	M.printf("length is: ");
+	M.printfEndl(x);
 
 	int* z = M.summVectors(c, b);//сложение векторов
-	printf("New vector is ");
-	printNewVector(z);
-	printfEndl(" ");
+	M.printf("New vector is ");
+	M.printNewVector(z);
+	M.printfEndl(" ");
 
 	int* normedVector = M.shortedNormed(c);//нормирование вектора с 
-	printf("normed vector is: ");
-	printNewVector(normedVector);
-	printfEndl("");
+	M.printf("normed vector is: ");
+	M.printNewVector(normedVector);
+	M.printfEndl("");
 
 	int scalarMult = M.scalMultiplication(c, b);//скалярное произведение векторов b и с 
-	printf("scalar multiplication is: ");
-	printf(scalarMult);
-	printfEndl("");
+	M.printf("scalar multiplication is: ");
+	M.printf(scalarMult);
+	M.printfEndl("");
 
 	int mixedMult = M.mixedMultiplication(c, b, d);//смешанное произведение
-	printf("mixed multiplication is : ");
-	printf(mixedMult);
-	printfEndl("");
+	M.printf("mixed multiplication is : ");
+	M.printf(mixedMult);
+	M.printfEndl("");
 
 	int* vectorMult = M.vectorMultiplication(c, b);//векторное произведение
-	printf("vector multiplication is :");
-	printNewVector(vectorMult);
-	printfEndl("");
+	M.printf("vector multiplication is :");
+	M.printNewVector(vectorMult);
+	M.printfEndl("");
 
 	int* tMultiplicated = M.someTMultiplication(4, d);//умножения вектора d на скаляр = 4
-	printf("t multiplicated on d is: ");
-	printNewVector(tMultiplicated);
-	printfEndl("");
+	M.printf("t multiplicated on d is: ");
+	M.printNewVector(tMultiplicated);
+	M.printfEndl("");
 
 	int* normalVector = M.findNormalVector(c, b);//нахождение нормального вектора
-	printf("normal vector is: ");
-	printNewVector(normalVector);
-	printfEndl("");
+	M.printf("normal vector is: ");
+	M.printNewVector(normalVector);
+	M.printfEndl("");
 };
